f004deffbe0684aea43a689b9687dbe7df0e1a08
MioAsuna <68692526+MioAsuna@users.noreply.github.com>
2025-08-07 17:29:32 +0300

    Update Http.cs

--- YoutubeDownloader.Core/Utils/Http.cs
+++ YoutubeDownloader.Core/Utils/Http.cs
@@ -1,24 +1,19 @@
-﻿using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Reflection;
+using System.Net.Http;
 
 namespace YoutubeDownloader.Core.Utils;
 
 public static class Http
 {
-    public static HttpClient Client { get; } =
-        new()
-        {
-            DefaultRequestHeaders =
-            {
-                // Required by some of the services we're using
-                UserAgent =
-                {
-                    new ProductInfoHeaderValue(
-                        "YoutubeDownloader",
-                        Assembly.GetExecutingAssembly().GetName().Version?.ToString(3)
-                    ),
-                },
-            },
-        };
+    public static HttpClient Client { get; } = Create();
+
+    private static HttpClient Create()
+    {
+        HttpClient Result = new();
+        Result.DefaultRequestHeaders.TryAddWithoutValidation(
+            "User-Agent",
+            "com.google.ios.youtube/19.45.4 (iPhone16,2; U; CPU iOS 18_1_0 like Mac OS X; US)"
+            // same as the one used in YoutubeExplode, as of right now
+        );
+        return Result;
+    }
 }
